0b90c724a15b656cd4501efd7cc1d8714a088689
Karen Marting <karenannemarting@yahoo.com>
2021-07-16 23:46:49 -0700

    forecast formatted in 5 columns

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-// const cities = ["London", "Paris", "New York", "california"];
-// const today = new Date();
-// const day = moment().format('dddd MMMM Do YYYY');
-
 
 const today = new Date();
 const day = moment().format('dddd MMMM Do YYYY');
 const key = 'f5c3aa4034bf39238e3769ad247499fd';
 const searchBtn = $('#searchBtn')
 const citySearch = $('#citySearch');
-const previousSearch = []
-
-function fiveDayForecast (event) {
-  const storedCitySearch = JSON.parse(localStorage.getItem('citySearch'))
-  storedCitySearch.innerText = ('')
-
-  // function storeCitySearch(){
-  // localStorage.setItem('citySearch', JSON.stringify(''))
-  // }
-  // storeCitySearch()
-  // get citysearch element by id 
-  // set the innerText equal to an empty string
+const pastCitySearchbtn = $('#pastCitysearch')
+  
+  function fiveDayForecast (event) {
   $.get({
     url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch.val() + '&appid=' + key + '&units=imperial'
   }).then(function(forecast) {
     // console.log (forecast.list)
-
     //for loop to pull 5 days of forecast 
-    
     for(let i = 4; i < forecast.list.length; i += 8){
-      const days = forecast.list[i];
-      // console.log('FORECAST OBJECT: ', forecast.list[i]);
-      // console.log('FORECAST TEMP DATA: ', forecast.list[i].main);
-      // console.log('FORECAST HUMIDITY DATA: ', forecast.list[i].main);
-
+      const days = forecast.list[i]; 
       // Create an element to hold the temp data:
-      
       const forecastContainer = document.getElementById("forecastContainer")
       const icon = days.weather[0].icon;
       const iconEl = document.createElement('img')
@@ -46,7 +26,7 @@ function fiveDayForecast (event) {
       // forecastContainer.appendChild(nextDayEl)
       const forecastTemp = (days.main.temp_min)
       const forecastTempEl = document.createElement('p')
-      forecastTempEl.innerText = 'Temp: ' + Math.floor(forecastTemp) + '°F'
+      forecastTempEl.innerText = 'Temp: ' + Math.floor(forecastTemp) + '°F' 
       forecastContainer.appendChild(forecastTempEl)
       const forecastHumidity = (days.main.humidity)
       const forecastHumidityEl = document.createElement('p')
@@ -56,8 +36,6 @@ function fiveDayForecast (event) {
       const forecastWindSpeedEl = document.createElement('p')
       forecastWindSpeedEl.innerText = 'Wind Speed: ' + forecastWindSpeed + 'mph'
       forecastContainer.appendChild(forecastWindSpeedEl)
-      
-
 }
 })
 }
@@ -68,7 +46,20 @@ function fiveDayForecast (event) {
 // })
 searchBtn.click(function (event) {
   event.preventDefault()
+ 
+   // citySearch.push(storedCityValue)
+  // storedCitySearch.innerText = (''
+
   fiveDayForecast (event)
+
+   //   const storedCitySearch = JSON.parse(localStorage.getItem('citySearch'))
+  //   if (storedCitySearch){
+  //   storedCitySearch.innerText =('')
+  
+  // localStorage.setItem('citySearch', citySearch.val())
+    // }
+  // get citysearch element by id 
+  // set the innerText equal to an empty string
   
   const weatherApiUrl= 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch.val() + '&appid=' + key + '&units=imperial'
     $.get({
@@ -95,10 +86,7 @@ searchBtn.click(function (event) {
         windSpeed.textContent = "Wind Speed: " + (data.wind.speed) + "mph";
         const uvIndex = document.getElementById('uvIndex');
         uvIndex.textContent = "UV Index: " + (data.uvi);    
-        const btn = document.createElement("button")
-        const weatherResults= document.getElementById("weatherSearchResults")
-        weatherResults.appendChild(btn)
-        btn.setAttribute("id", "saveSearchBtn")
+        
 
 
         //set ID attribute to the button globally 
@@ -107,7 +95,7 @@ searchBtn.click(function (event) {
               }
             )
           
-          
+            
           }
     )
     
